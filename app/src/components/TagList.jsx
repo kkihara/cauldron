4a8d09dda9c0ad9c7f7e0e2f6a03085a614b1eef
kenny <kkihara1@gmail.com>
2019-05-18 14:01:11 -0700

    update tag style

--- app/src/components/TagList.jsx
+++ app/src/components/TagList.jsx
@@ -2,60 +2,32 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import Chip from '@material-ui/core/Chip';
 import AddTag from '../containers/AddTag';
 import type { T_Tag } from '../types';
 // import img from '../../assets/delete-icon.png';
 
 type Props = {
   tags: Array<T_Tag>,
+  isHome: bool,
   searchTag: any,
+  deleteTag: any,
 };
 
-const List = styled.ul`
-  list-style-type: none;
-  margin: 0px;
-`;
-
-const TagElement = styled.li`
-  display: inline-block;
-  padding: 2px;
-  cursor: pointer;
-`;
-
-const Tag = styled.p`
-  border-radius: 10px / 7px;
-  border-color: #000000;
-  border-style: solid;
-  padding: 7px;
-  list-style-position: inside;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  max-width: 50px;
-  font-size: 12px;
-`;
-
-// const RemoveButton = styled.img`
-//   height: 10px;
-//   width: 10px;
-//   padding-left: 2px;
-//   float: left;
-// `;
-
-const TagList = ({ tags, isHome, searchTag }: Props) => {
+const TagList = ({ tags, isHome, searchTag, deleteTag }: Props) => {
   if (isHome) {
     return (
       <div>
         <AddTag/>
-        <List>
-          {tags.map((tag, idx) =>
-            <TagElement key={ idx } onClick={ () => searchTag(tag.content) }>
-              <Tag>
-                { tag.content }
-              </Tag>
-            </TagElement>
-          )}
-        </List>
+        {tags.map((tag, idx) =>
+          <Chip
+            key={ idx }
+            label={ tag.content }
+            onClick={ () => searchTag(tag.content) }
+            onDelete={ () => deleteTag(tag.id) }
+            clickable={ true }
+          />
+        )}
       </div>
     )
   } else {
--- app/src/containers/TagListContainer.js
+++ app/src/containers/TagListContainer.js
@@ -2,16 +2,26 @@
 
 import { connect } from 'react-redux';
 import { appendToSearch } from '../actions/page';
+import { deleteTag } from '../actions/tag';
 import TagList from '../components/TagList';
 import { viewTypes } from '../types';
 
 const mapStateToProps = state => ({
   tags: state.tagList[state.currentPage.id] || [],
   isHome: state.currentView.value == viewTypes.home,
+  pageId: state.currentPage.id,
 });
 
 const mapDispatchToProps = dispatch => ({
-  searchTag: (tag: string) => dispatch(appendToSearch('#' + tag))
-})
+  searchTag: (tag: string) => dispatch(appendToSearch('#' + tag)),
+  deleteTag: (pageId: string, tagId: string) => dispatch(deleteTag(pageId, tagId)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(TagList);
+const mergeProps = (propsFromState, propsFromDispatch) => ({
+  tags: propsFromState.tags,
+  isHome: propsFromState.isHome,
+  searchTag: propsFromDispatch.searchTag,
+  deleteTag: (tagId: string) => propsFromDispatch.deleteTag(propsFromState.pageId, tagId),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(TagList);
